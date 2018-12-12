2ce093852635845aaa83f4290d6cc0c68dbea7b2
Jenelle Kaldon <JenelleKaldon@gmail.com>
2018-12-11 21:35:46 -0600

    Refactored WordListGeneratorService to break up code into multiple functions

--- src/app/word-list-generator.service.ts
+++ src/app/word-list-generator.service.ts
@@ -1,19 +1,30 @@
 
 export class WordListGeneratorService {
   constructor() { }
+
   public static generateWordList(rows: number, cols: number) {
-    // console.log("In generateWordList()")
-    const numSquares: number = rows * cols;
-    const numWords = Math.floor(numSquares / 2);
-    if (numWords * 2 !== numSquares) {
-      alert('Rows * Cols must be an even number');
-    }
+    const numWords = this.getNumWords(rows, cols);
+
     let wordList: any[] = [];
     for (let i = 0; i < numWords; i++) {
       wordList.push(i);
     }
     wordList = wordList.concat(wordList);
 
+    return this.randomizeWordList(wordList);
+  }
+
+  public static getNumWords(rows: number, cols: number) {
+    // console.log("In getNumWords()")
+    const numSquares: number = rows * cols;
+    const numWords = Math.floor(numSquares / 2);
+    if (numWords * 2 !== numSquares) {
+      alert('Rows * Cols must be an even number');
+    }
+    return numWords;
+  }
+
+  private static randomizeWordList(wordList: any[]) {
     // rendomize wordList
     for (let i = wordList.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
